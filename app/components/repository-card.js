4cd35cb20bbff1427285c94f8b45be2f272d6158
Tony Ward <tony.ward@toyotaconnected.com>
2018-05-10 17:30:39 -0500

    Showed comparison of old syntax

--- app/components/repository-card.js
+++ app/components/repository-card.js
@@ -1,27 +1,28 @@
-import { assert } from '@ember/debug';
+import { argument } from '@ember-decorators/argument';
 import { computed } from '@ember-decorators/object';
+import { required } from '@ember-decorators/argument/validation';
 import { tagName, classNames } from '@ember-decorators/component';
+import { type } from '@ember-decorators/argument/type';
 import Component from '@ember/component';
 
 @classNames('repository-card p-3')
 @tagName('div')
 export default class RepositoryCard extends Component {
-  // TODO: Could try out this argument addon too
-  // https://github.com/ember-decorators/argument
 
-  didInsertElement() {
-    super.didInsertElement(...arguments);
+  @required
+  @argument
+  @type('string')
+  name = null;
 
-    const {
-      name,
-      stars,
-      forks
-    } = this;
+  @required
+  @argument
+  @type('number')
+  stars = 0;
 
-    assert('name is required', name);
-    assert('stars must be greater than or equal to 0', stars >= 0);
-    assert('forks must be greater than or equal to 0', forks >= 0);
-  }
+  @required
+  @argument
+  @type('number')
+  forks = 0;
 
   @computed('stars')
   get starredTheme() {
@@ -29,3 +30,28 @@ export default class RepositoryCard extends Component {
     return stars > 50 ? 'text-red-light' : 'text-blue-light';
   }
 }
+
+// COMPARISON PRIOR TO UPGRADE
+
+// export default Component.extend({
+//  tagName: 'div',
+//  classNames: [ 'repository-card', 'p-3' ],
+//
+//  name: null,
+//  stars: 0,
+//  forks: 0,
+//
+//  starredTheme: computed('stars', function() {
+//    const { stars } = this;
+//    return stars > 50 ? 'text-red-light' : 'text-blue-light';
+//  }),
+//
+//  didInsertElement() {
+//    // Validate name, stars, forks
+//    const { name, stars, forks } = this;
+//    assert('name is required', name);
+//    assert('stars must be a number', !isNaN(stars));
+//    assert('forks must be a number', !isNaN(forks));
+//  }
+// });
+
--- tests/integration/components/repository-card-test.js
+++ tests/integration/components/repository-card-test.js
@@ -6,8 +6,6 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | repository-card', function(hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: For some reason ember-qunit-assert-helpers isn't catching the assertion
-  //       when we attempt assert.expectAssertion(...) ?
   test('it renders', async function(assert) {
     await render(hbs`{{repository-card name='ynotdraw' stars=0 forks=0}}`);
 
